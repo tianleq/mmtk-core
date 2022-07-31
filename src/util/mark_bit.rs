62e2c3badfe4494de364530986807ae70905d7f8
Tianle Qiu <tianle.qiu@anu.edu.au>
2022-07-31 17:18:01 +1000

    implement transitive closure in pre-write barrier

---
+++ src/util/mark_bit.rs
@@ -0,0 +1,36 @@
+use crate::util::metadata::side_metadata;
+use crate::util::metadata::side_metadata::SideMetadataSpec;
+use crate::util::ObjectReference;
+use atomic::Ordering;
+
+/// An mark-bit is required per min-object-size aligned address , rather than per object, and can only exist as side metadata.
+pub(crate) const GLOBAL_MARK_SIDE_METADATA_SPEC: SideMetadataSpec =
+    crate::util::metadata::side_metadata::spec_defs::GLOBAL_MARK_BIT;
+
+pub fn set_global_mark_bit(object: ObjectReference) {
+    // debug_assert!(!is_global_mark_set(object), "{:x}: mark bit already set", object);
+    side_metadata::store_atomic(
+        &GLOBAL_MARK_SIDE_METADATA_SPEC,
+        object.to_address(),
+        1,
+        Ordering::SeqCst,
+    );
+}
+
+pub fn unset_global_mark_bit(object: ObjectReference) {
+    // debug_assert!(is_global_mark_set(object), "{:x}: mark bit not set", object);
+    side_metadata::store_atomic(
+        &GLOBAL_MARK_SIDE_METADATA_SPEC,
+        object.to_address(),
+        0,
+        Ordering::SeqCst,
+    );
+}
+
+pub fn is_global_mark_set(object: ObjectReference) -> bool {
+    side_metadata::load_atomic(
+        &GLOBAL_MARK_SIDE_METADATA_SPEC,
+        object.to_address(),
+        Ordering::SeqCst,
+    ) == 1
+}
